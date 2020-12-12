5afc3125f8c98bca6a15d4943ae57c2e00fcf2c2
Monzer Abdullaziz <nezonru87@yahoo.com>
2020-12-12 18:19:57 +0200

    may be final

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //Setup Express
 const express = require('express');
 const app = express();
+var cors = require('cors')
 const db = require('./config/database');
 const port = 3232;
 app.use(express.json());
-
+app.use(cors())
 const sql = require("mssql");
 
 // DB credentials
@@ -18,13 +19,179 @@ const config = {
     }
 }
 
-//Routers
-// const auth = require('./Auth/index');
+//Query Builders
+function buildInsertQuery(query , fields , values){
+    for (let index = 0; index <= fields.length-1; index++){
+        if(index == 0 && index == fields.length-1){
+            let item = '(' + fields[index] + ') '; 
+            query += item; 
+        } else {
+            if (index == 0){
+                let item = '( ' + fields[index] + ', '; 
+                query += item; 
+              } else if (index != 0 && index != fields.length-1){
+                  let midItem = fields[index] + ', ';
+                  query += midItem;
+              } else if (index == fields.length-1){
+                  let lastItem = fields[index] + ' )';
+                  query+= lastItem;
+              }
+        }
+        
+    }
+query += ' VALUES ';
+
+    for (let index = 0; index <= values.length-1; index++){
+        if(index == 0 && index == values.length-1){
+            let item = `('${values[index]}') `; 
+            query += item; 
+        } else {
+            if (index == 0){
+                let item = `( '${values[index]}' , `; 
+                query += item; 
+              } else if (index != 0 && index != values.length-1){
+                  let midItem = ` '${values[index]}' , `;
+                  query += midItem;
+              } else if (index == values.length-1){
+                  let lastItem =` '${values[index]}' )`;
+                  query+= lastItem;
+              }
+        }
+    }
+console.log(query)
+return query;
+}
+
+function buildSelectWhereQuery(query , fields , values){
+    let part = [];
+    let tempPart = '';
+    let partString = '';
+    
+        for(let index = 0; index <= fields.length-1; index++){
+            for(let dex = 0; dex <= values.length-1; dex++){
+                if (index == dex){
+                    tempPart = `${fields[index]} = '${values[dex]}' `
+                    part.push(tempPart);
+                }
+            }
+        }
+    
+        for(let ind = 0; ind <= part.length-1; ind++){
+            if(ind == 0 && ind == values.length-1){
+                partString += `${query} ${part[ind]}`;
+            }else {
+                if (ind == 0 && values.length != 1){
+                    partString += `${query} ${part[ind]} and`;
+                } else if (ind != 0 && ind != values.length-1){
+                    partString += ` ${part[ind]} and`;
+                } else if (ind == values.length-1){
+                    partString += ` ${part[ind]}`;
+                }
+            }
+    
+        }
+    console.log(partString)
+    return partString;
+    
+    }
+
+    function buildUpdateQuery(query , fields , values , condFields , condValues){
+        let part = [];
+        let tempPart = '';
+        let partString = '';
+        let fullPart = [];
+        let fullString = '';
+            for(let index = 0; index <= fields.length-1; index++){
+                for(let dex = 0; dex <= values.length-1; dex++){
+                    if (index == dex){
+                        tempPart = `${fields[index]} = '${values[dex]}' `
+                        part.push(tempPart);
+                    }
+                }
+            }
+        
+            for(let ind = 0; ind <= part.length-1; ind++){
+                if(ind == 0 && ind == values.length-1){
+                    partString += `${part[ind]}`;
+                }else {
+                    if (ind == 0 && values.length != 1){
+                        partString += `${query} ${part[ind]} ,`;
+                    } else if (ind != 0 && ind != values.length-1){
+                        partString += ` ${part[ind]} ,`;
+                    } else if (ind == values.length-1){
+                        partString += ` ${part[ind]}`;
+                    }
+                }
+        
+            }
+    
+            fullString = partString + 'WHERE ';
+    
+            for(let index = 0; index <= condFields.length-1; index++){
+                for(let dex = 0; dex <= condValues.length-1; dex++){
+                    if (index == dex){
+                        tempPart = `${condFields[index]} = '${condValues[dex]}' `
+                        fullPart.push(tempPart);
+                    }
+                }
+            }
+        
+            for(let ind = 0; ind <= fullPart.length-1; ind++){
+                if(ind == 0 && ind == condValues.length-1){
+                    fullString += `${fullPart[ind]}`;
+                }else {
+                    if (ind == 0 && condValues.length != 1){
+                        fullString += `${fullPart[ind]} and`;
+                    } else if (ind != 0 && ind != condValues.length-1){
+                        fullString += ` ${fullPart[ind]} and`;
+                    } else if (ind == condValues.length-1){
+                        fullString += ` ${fullPart[ind]}`;
+                    }
+                }
+        
+            }
+    
+    console.log(fullString)
+        return fullString;
+        
+        }
+
+        function buildDeleteQuery(query , fields , values){
+            let part = [];
+            let tempPart = '';
+            let partString = '';
+            
+                for(let index = 0; index <= fields.length-1; index++){
+                    for(let dex = 0; dex <= values.length-1; dex++){
+                        if (index == dex){
+                            tempPart = `${fields[index]} = '${values[dex]}' `
+                            part.push(tempPart);
+                        }
+                    }
+                }
+            
+                for(let ind = 0; ind <= part.length-1; ind++){
+                    if(ind == 0 && ind == values.length-1){
+                        partString += `${query} ${part[ind]}`;
+                    }else {
+                        if (ind == 0 && values.length != 1){
+                            partString += `${query} ${part[ind]} and`;
+                        } else if (ind != 0 && ind != values.length-1){
+                            partString += ` ${part[ind]} and`;
+                        } else if (ind == values.length-1){
+                            partString += ` ${part[ind]}`;
+                        }
+                    }
+            
+                }
+            console.log(partString);
+            return partString;
+            
+            }
+
 //0923595393
 //1234
-//Routers Setup
-// app.use('/keeti/users' , auth);
-
+//Routers
 app.get('/keeti/auth', (req, res) => {
    async function connectDB() {
         const pool = new sql.ConnectionPool(config);
@@ -71,9 +238,197 @@ app.get('/keeti/auth', (req, res) => {
     execute();
 });
 
-//app.get('*', (req, res) => {
-//   res.status(404).json({error : 'Not Found!'});
-//});
+app.get('/keeti/sw' , (req , res)=>{
+    let guery = buildSelectWhereQuery(req.query.que ,req.query.field , req.query.value);
+        async function connectDB() {
+             const pool = new sql.ConnectionPool(config);
+         
+             try {
+                 await pool.connect();
+                 console.log('Connected to database');
+         
+                 return pool;
+             }
+             catch(err) {
+                 console.log('Database connection failed!', err);
+         
+                 return err;
+             }
+         }
+         
+         async function getAll() {
+             const DB = await connectDB();
+         
+             try {
+                 const result = await DB.request().query(guery);
+         
+                 return result.recordset;
+             }
+             catch (err) {
+                 console.log('Error querying database', err);
+         
+                 return err;
+             }
+             finally {
+                 DB.close();
+             }
+         }
+         
+         async function execute() {
+             let result = await getAll();
+             JSON.stringify(result)
+         
+             res.status(200).json(result)
+             // return JSON.stringify(result);
+         }
+         
+         execute();
+});
+
+app.post('/keeti/is' , (req , res)=>{
+    let guery = buildInsertQuery(req.query.que ,req.query.field , req.query.value);
+        async function connectDB() {
+             const pool = new sql.ConnectionPool(config);
+         
+             try {
+                 await pool.connect();
+                 console.log('Connected to database');
+         
+                 return pool;
+             }
+             catch(err) {
+                 console.log('Database connection failed!', err);
+         
+                 return err;
+             }
+         }
+         
+         async function getAll() {
+             const DB = await connectDB();
+         
+             try {
+                 const result = await DB.request().query(guery);
+         
+                 return result.recordset;
+             }
+             catch (err) {
+                 console.log('Error querying database', err);
+         
+                 return err;
+             }
+             finally {
+                 DB.close();
+             }
+         }
+         
+         async function execute() {
+             let result = await getAll();
+             JSON.stringify(result)
+         
+             res.status(200).json(result)
+             // return JSON.stringify(result);
+         }
+         
+         execute();
+});
+
+app.put('/keeti/uq' , (req , res)=>{
+    let guery = buildUpdateQuery(req.query.que ,req.query.field , req.query.value , req.query.condf , req.query.condv);
+        async function connectDB() {
+             const pool = new sql.ConnectionPool(config);
+         
+             try {
+                 await pool.connect();
+                 console.log('Connected to database');
+         
+                 return pool;
+             }
+             catch(err) {
+                 console.log('Database connection failed!', err);
+         
+                 return err;
+             }
+         }
+         
+         async function getAll() {
+             const DB = await connectDB();
+         
+             try {
+                 const result = await DB.request().query(guery);
+         
+                 return result.recordset;
+             }
+             catch (err) {
+                 console.log('Error querying database', err);
+         
+                 return err;
+             }
+             finally {
+                 DB.close();
+             }
+         }
+         
+         async function execute() {
+             let result = await getAll();
+             JSON.stringify(result)
+         
+             res.status(200).json(result)
+             // return JSON.stringify(result);
+         }
+         
+         execute();
+});
+
+app.delete('/keeti/dq', (req , res)=>{
+    let guery = buildDeleteQuery(req.query.que ,req.query.field , req.query.value);
+        async function connectDB() {
+             const pool = new sql.ConnectionPool(config);
+         
+             try {
+                 await pool.connect();
+                 console.log('Connected to database');
+         
+                 return pool;
+             }
+             catch(err) {
+                 console.log('Database connection failed!', err);
+         
+                 return err;
+             }
+         }
+         
+         async function getAll() {
+             const DB = await connectDB();
+         
+             try {
+                 const result = await DB.request().query(guery);
+         
+                 return result.recordset;
+             }
+             catch (err) {
+                 console.log('Error querying database', err);
+         
+                 return err;
+             }
+             finally {
+                 DB.close();
+             }
+         }
+         
+         async function execute() {
+             let result = await getAll();
+             JSON.stringify(result)
+         
+             res.status(200).json(result)
+             // return JSON.stringify(result);
+         }
+         
+         execute();
+});
+
+app.get('*', (req, res) => {
+  res.status(404).json({error : 'Not Found!'});
+});
 
 app.post('*', (req, res) => {
     res.status(404).json({error : 'Not Found!'});
@@ -88,4 +443,4 @@ app.delete('*', (req, res) => {
 });
 
 //listening at 3000
-app.listen(process.env.PORT);
+app.listen(3000);
--- test.js
+++ test.js
@@ -1,32 +1,45 @@
-let que = 'INSERT INTO table_name ';
-let field = ['name' , 'phone' , 'level'];
-let value = ['monzer' , '0121601505' , 'pre'];
+let que = 'DELETE FROM SET_users WHERE ';
+let field = ['nameEN' , 'nameAR' , 'password' , 'type', 'status' , 'phone'];
+let value = ['monzer' , '---' , 123, 'none' , 'Enabled' , '0121601505'];
+let condField = ['nameEN' , 'nameAR'];
+let condValue = ['monzer' , '---'];
 
 function buildInsertQuery(query , fields , values){
     for (let index = 0; index <= fields.length-1; index++){
-        if (index == 0){
-          let item = '( ' + fields[index] + ', '; 
-          query += item; 
-        } else if (index != 0 && index != fields.length-1){
-            let midItem = fields[index] + ', ';
-            query += midItem;
-        } else if (index == fields.length-1){
-            let lastItem = fields[index] + ' )';
-            query+= lastItem;
+        if(index == 0 && index == fields.length-1){
+            let item = '(' + fields[index] + ') '; 
+            query += item; 
+        } else {
+            if (index == 0){
+                let item = '( ' + fields[index] + ', '; 
+                query += item; 
+              } else if (index != 0 && index != fields.length-1){
+                  let midItem = fields[index] + ', ';
+                  query += midItem;
+              } else if (index == fields.length-1){
+                  let lastItem = fields[index] + ' )';
+                  query+= lastItem;
+              }
         }
+        
     }
 query += ' VALUES ';
 
     for (let index = 0; index <= values.length-1; index++){
-        if (index == 0){
-          let item = '( "' + values[index] + '" , '; 
-          query += item; 
-        } else if (index != 0 && index != values.length-1){
-            let midItem = ' "' + values[index] + '" , ';
-            query += midItem;
-        } else if (index == values.length-1){
-            let lastItem =' "'+ values[index] + '" )';
-            query+= lastItem;
+        if(index == 0 && index == values.length-1){
+            let item = `('${values[index]}') `; 
+            query += item; 
+        } else {
+            if (index == 0){
+                let item = `( '${values[index]}' , `; 
+                query += item; 
+              } else if (index != 0 && index != values.length-1){
+                  let midItem = ` '${values[index]}' ,  `;
+                  query += midItem;
+              } else if (index == values.length-1){
+                  let lastItem =` '${values[index]}' )`;
+                  query+= lastItem;
+              }
         }
     }
 
@@ -41,25 +54,149 @@ let partString = '';
     for(let index = 0; index <= fields.length-1; index++){
         for(let dex = 0; dex <= values.length-1; dex++){
             if (index == dex){
-                tempPart = `${fields[index]} = "${values[dex]}" `
+                tempPart = `${fields[index]} = '${values[dex]}' `
                 part.push(tempPart);
             }
         }
     }
 
     for(let ind = 0; ind <= part.length-1; ind++){
-
-        if (ind == 0){
-            partString += `${query} ${part[ind]}`;
-        } else if (ind != 0 && ind != values.length-1){
+        if(ind == 0 && ind == values.length-1){
             partString += `${part[ind]}`;
-        } else if (ind == values.length-1){
-            partString += `${part[ind]}`;
+        }else {
+            if (ind == 0 && values.length != 1){
+                partString += `${query} ${part[ind]} and`;
+            } else if (ind != 0 && ind != values.length-1){
+                partString += ` ${part[ind]} and`;
+            } else if (ind == values.length-1){
+                partString += ` ${part[ind]}`;
+            }
         }
+
     }
 
 return partString;
 
 }
 
-console.log(`you insert query to the function like this: \n\n ${que} \n\n you insert fields to the function like this:\n\n ${field} \n\n you insert values to the function like this:\n\n ${value}\n\n the output in buildSelectWhereQuery will be: \n\n ${buildSelectWhereQuery(que , field , value)} \n\n the output in buildSelectWhereQuery will be: \n\n ${buildInsertQuery(que , field , value)}`)
+function buildUpdateQuery(query , fields , values , condFields , condValues){
+    let part = [];
+    let tempPart = '';
+    let partString = '';
+    let fullPart = [];
+    let fullString = '';
+        for(let index = 0; index <= fields.length-1; index++){
+            for(let dex = 0; dex <= values.length-1; dex++){
+                if (index == dex){
+                    tempPart = `${fields[index]} = '${values[dex]}' `
+                    part.push(tempPart);
+                }
+            }
+        }
+    
+        for(let ind = 0; ind <= part.length-1; ind++){
+            if(ind == 0 && ind == values.length-1){
+                partString += `${part[ind]}`;
+            }else {
+                if (ind == 0 && values.length != 1){
+                    partString += `${query} ${part[ind]} ,`;
+                } else if (ind != 0 && ind != values.length-1){
+                    partString += ` ${part[ind]} ,`;
+                } else if (ind == values.length-1){
+                    partString += ` ${part[ind]}`;
+                }
+            }
+    
+        }
+
+        fullString = partString + 'WHERE ';
+
+        for(let index = 0; index <= condFields.length-1; index++){
+            for(let dex = 0; dex <= condValues.length-1; dex++){
+                if (index == dex){
+                    tempPart = `${condFields[index]} = '${condValues[dex]}' `
+                    fullPart.push(tempPart);
+                }
+            }
+        }
+    
+        for(let ind = 0; ind <= fullPart.length-1; ind++){
+            if(ind == 0 && ind == condValues.length-1){
+                fullString += `${fullPart[ind]}`;
+            }else {
+                if (ind == 0 && condValues.length != 1){
+                    fullString += `${fullPart[ind]} and`;
+                } else if (ind != 0 && ind != condValues.length-1){
+                    fullString += ` ${fullPart[ind]} and`;
+                } else if (ind == condValues.length-1){
+                    fullString += ` ${fullPart[ind]}`;
+                }
+            }
+    
+        }
+
+
+    return fullString;
+    
+    }
+
+    function buildDeleteQuery(query , fields , values){
+        let part = [];
+        let tempPart = '';
+        let partString = '';
+        
+            for(let index = 0; index <= fields.length-1; index++){
+                for(let dex = 0; dex <= values.length-1; dex++){
+                    if (index == dex){
+                        tempPart = `${fields[index]} = '${values[dex]}' `
+                        part.push(tempPart);
+                    }
+                }
+            }
+        
+            for(let ind = 0; ind <= part.length-1; ind++){
+                if(ind == 0 && ind == values.length-1){
+                    partString += `${part[ind]}`;
+                }else {
+                    if (ind == 0 && values.length != 1){
+                        partString += `${query} ${part[ind]} and`;
+                    } else if (ind != 0 && ind != values.length-1){
+                        partString += ` ${part[ind]} and`;
+                    } else if (ind == values.length-1){
+                        partString += ` ${part[ind]}`;
+                    }
+                }
+        
+            }
+        
+        return partString;
+        
+        }
+
+
+console.log(`
+you insert query to the function like this:
+${que}
+you insert fields to the function like this:
+${field}
+you insert values to the function like this:
+${value}
+the output in buildSelectWhereQuery will be:
+${buildSelectWhereQuery(que , field , value)}
+the output in buildSelectWhereQuery will be:
+${buildInsertQuery(que , field , value)}
+condField for update query builder is:
+${condField}
+condValues for update query builder is:
+${condValue}
+Result of The BuildUpdateQuery function:
+${buildUpdateQuery(que , field , value , condField , condValue)}
+Result of The BuildDeleteQuery function:
+${buildDeleteQuery(que , field , value)}
+`)
+
+//DELETE FROM table_name WHERE condition;
+//UPDATE table_name SET column1 = value1, column2 = value2, ... WHERE condition;
+//INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);
+//value[]
+//que
