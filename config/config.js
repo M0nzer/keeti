a19a603dbf0d06223b2533a32a89174c0a46905c
Monzer Abdullaziz <nezonru87@yahoo.com>
2021-03-30 19:25:04 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,13 +2,13 @@
 // config
 module.exports = {
 database : {
-    user: 'citcsudan2_SQLLogin_1',
-    password: '12345asdfg',
-    server: 'jazatt2020.mssql.somee.com',
-    database: 'jazatt2020',
+    user: '',
+    password: '',
+    server: '',
+    database: '',
     options: {
         encrypt: true
     }
 },
-secrut : 'omerkeeti'
+secrut : ''
 }
